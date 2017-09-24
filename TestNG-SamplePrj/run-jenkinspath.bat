169e03840649018052e866de9c969e9012799279
venkatvalavala <vvrvalavala@gmail.com>
2017-09-25 00:12:44 +0530

    save patg

--- TestNG-SamplePrj/run-jenkinspath.bat
+++ TestNG-SamplePrj/run-jenkinspath.bat
@@ -1,4 +1,4 @@
-set projectLocation=C:\Program Files (x86)\Jenkins\workspace\GiJenkins-TestNGSamplePrj\TestNG-SamplePrj
+set projectLocation=C:\Program Files (x86)\Jenkins\workspace\GiJenkins-TestNGSamplePrj\TestNG-SamplePrj\
 cd %projectLocation%
 set classpath=%projectLocation%\bin;%projectLocation%\lib\*
 java org.testng.TestNG %projectLocation%\testng.xml
